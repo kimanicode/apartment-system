1fb58af9effb4c3dce0391463ee7c5eef3b437a9
kimanicode <karaba.dev@gmail.com>
2022-07-14 11:01:01 +0300

    modified git ignore

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 function Footer() {
   return (
     <div className='footer'>
-        Developed by <a href='#'>Kimani</a>
+        Developed by <a href='#'>grace</a>
         
     
     </div>
